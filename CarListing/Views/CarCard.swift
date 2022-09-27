cda8c2dd3ad38561069ed5f24c6dad4170ea132a
KenzieMac <kenziemacd@icloud.com>
2022-09-27 13:14:33 -0700

    Layout one version of initial cardUI

--- CarListing/Views/CarCard.swift
+++ CarListing/Views/CarCard.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct CarCard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .center) {
+            Image("Tacoma")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 250)
+            VStack(alignment: .leading) {
+                Text("Car Name")
+                    .bold()
+                Text("Car Price")
+                    .font(.caption)
+                Text("Car Rating")
+                    .padding(.top, 20)
+            }
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, alignment: .center)
+        .cornerRadius(10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
+        )
     }
 }
 
