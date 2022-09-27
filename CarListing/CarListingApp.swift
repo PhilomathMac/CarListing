be86a1e138a5128949e1fa895326c7cbfd9d119c
KenzieMac <kenziemacd@icloud.com>
2022-09-27 11:13:24 -0700

    Initial Commit

---
+++ CarListing/CarListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarListingApp.swift
+//  CarListing
+//
+//  Created by McKenzie Macdonald on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct CarListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
